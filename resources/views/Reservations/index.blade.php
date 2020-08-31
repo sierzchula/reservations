42a4210bcbf1e63c29f393347800f99d84f24da6
Michał Sierzchuła <michal@webbest.pl>
2020-08-31 11:29:09 +0200

    view style

--- resources/views/Reservations/index.blade.php
+++ resources/views/Reservations/index.blade.php
@@ -3,21 +3,21 @@
 @section("content")
 <p>reservations - <a href="{{ route('reservations.create') }}">add new</a></p>
 <div class="d-flex flex-row">
-    <div class="" style="margin-right:300px"></div>
+    <div class="" style="margin-right:150px"></div>
         @for($i=0;$i<=31;$i++)
-            <div class="btn" style="min-width:34px">{{$i}}</div>
+            <div class="" style="width:30px;text-align:center">{{$i}}</div>
         @endfor
 </div>
 
 <div class="d-flex">
     <div class="d-flex flex-column">
         @foreach ( $reservations as $key =>$reservations_aparment )
-            <div class="p-2 no-wrap border border-dark" style="width:300px;white-space: nowrap;background:white">{{ $key }}</div>
+            <div class="p-2 no-wrap border border-dark" style="width:150px;white-space: nowrap;background:white;height:41px;overflow:hidden" title="{{ $key }}">{{ $key }}</div>
         @endforeach
     </div>
-    <div class="d-flex flex-column" style="position:relative;background-size: 34px 40px;background-image:linear-gradient(to right, grey 1px, transparent 1px),linear-gradient(to bottom, grey 1px, transparent 1px);">
+    <div class="d-flex flex-column" style="overflow: hidden;position:relative;background-size: 30px 41px;background-image:linear-gradient(to right, grey 1px, transparent 1px),linear-gradient(to bottom, grey 1px, transparent 1px);">
         @foreach ( $reservations as $key =>$reservations_aparment )
-            <div class="d-flex flex-nowrap p-1" style="position:relative;width:{{32*34}}px;height:40px">
+            <div class="d-flex flex-nowrap p-1" style="position:relative;width:{{32*30}}px;height:41px">
 
                 @foreach ( $reservations_aparment as $reservation)
                     @if ( $reservation['days'] > 0 )
@@ -45,7 +45,7 @@
                                 @default
                                     bg-primary text-white
                             @endswitch
-                        " style="height:34px;position:absolute;left: {{ 20 + date('d', $reservation['start_date']) * 34 }}px;width: {{ $reservation['days'] * 34 }}px">{{ $reservation['days'] }}</a>
+                        " style="height:34px;position:absolute;left: {{ 20 + date('d', $reservation['start_date']) * 30 }}px;width: {{ $reservation['days'] * 30 }}px">{{ $reservation['days'] }}</a>
                     @endif
                 @endforeach
             </div>
@@ -62,7 +62,7 @@
             
                 
                 
-        </div>
+        
     @endforeach
    
 </div>
