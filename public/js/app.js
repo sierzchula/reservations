62c980786a5dfc786172ecc02bdca59886c805ed
Michał Sierzchuła <michal@webbest.pl>
2020-09-03 14:27:12 +0200

    js css compiled files

--- public/js/app.js
+++ public/js/app.js
@@ -52134,8 +52134,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/reservations/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/reservations/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\wamp64\www\reservations\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\wamp64\www\reservations\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
